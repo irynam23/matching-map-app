9cb2671527b7ef0240b5f429777f3614925245e1
Iryna <Iryna@MacBook-Air-MacBook.local>
2023-05-18 12:47:45 +0300

    fix basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="matching-map-app/">
+    <BrowserRouter basename="/matching-map-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
